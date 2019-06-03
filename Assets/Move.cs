78c7e61be0035b09451ee1acb3e7d93c8e7e91ba
Kathleen Bryan <kathleenbryan24@gmail.com>
2019-06-03 08:11:27 -0400

    initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class Move : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject myObject; 
+
+    [SerializeField]
+    public ParticleSystem myParticles; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private bool particleSystemRunning = false;
+    public void ReceivePosition(Vector3 position) {
+     
+        Debug.Log(position.ToString()); 
+        Debug.Log("This is position we are using" + position); 
+       
+        myObject.transform.position = position;
+
+
+      /* if(MLHandKeyPose.Fist) {
+          Debug.Log("Fist gesture"); 
+          myParticles.Stop(); 
+       } else {
+         myParticles.ParticleSystem.Play(); 
+       } */ 
+
+      if ( MLHands.Left.KeyPose   == MLHandKeyPose.Fist) {
+        Debug.Log("user made fist"); 
+        if(particleSystemRunning){
+          myParticles.Stop(); 
+          particleSystemRunning = false;
+        }
+    } else if (MLHands.Left.KeyPose == MLHandKeyPose.OpenHandBack) {
+       Debug.Log("user opened hands"); 
+       if(!particleSystemRunning){
+          myParticles.Play();
+          particleSystemRunning = true;
+       } 
+    } else {
+      Debug.Log("User hand = " + MLHands.Left.KeyPose.ToString());
+    }
+
+
+/* 
+    void IEnumerator PlayParticleSystem() {
+
+      yield new return WaitForSeconds(0); 
+      particleSystemPlaying = true; 
+      myParticles.Play(); 
+
+
+    } */ 
+
+     
+}
+}
---
+++ Assets/TouchpadGestures.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.MagicLeap;
+
+public class TouchpadGestures : MonoBehaviour {
+
+    #region Public Variables
+    public Text typeText, stateText, directionText;
+    public Camera Camera;
+    #endregion
+
+    #region Private Variables
+    private MLInputController _controller;
+    #endregion
+
+    #region Unity Methods
+    void Start() {
+        MLInput.Start();
+        _controller = MLInput.GetController(MLInput.Hand.Left);
+    }
+
+    void OnDestroy() {
+        MLInput.Stop();
+    }            
+
+    void Update() {
+        updateTransform();
+        updateGestureText();
+
+        if (_controller.TouchpadGesture.Type.ToString() != null) {
+
+            switch (_controller.TouchpadGesture.Type.ToString())
+        {
+            case "Swipe":
+                typeText.text = "this works"; 
+                break;
+            case "RadialScroll":
+                typeText.text = "this also works"; 
+                break; 
+            default:
+                break;
+        } 
+ 
+           /*  if (_controller.TouchpadGesture.Type.ToString()  == "Swipe") {
+                Debug.Log("gets input " + _controller.TouchpadGesture.Type.ToString());        
+                 typeText.text = "this works"; 
+            }  */ 
+        }
+
+    
+     /* switch (_controller.TouchpadGesture.Type.ToString())
+        {
+            case "Swipe":
+                typeText.text = "this works"; 
+                break;
+            case "RadialScroll":
+                typeText.text = "this also works"; 
+                break; 
+            default:
+                break;
+        }   */ 
+                    
+
+            //typeText.text = "A gesture was just made : " +  _controller.TouchpadGesture.Type.ToString(); 
+
+        
+    }
+
+
+    #endregion
+
+    #region Private Methods
+    void updateGestureText() {
+
+        string gestureType = _controller.TouchpadGesture.Type.ToString();
+        string gestureState = _controller.TouchpadGestureState.ToString();
+        string gestureDirection = _controller.TouchpadGesture.Direction.ToString();
+
+       // typeText.text = "Type: " + gestureType;
+        stateText.text = "State: " + gestureState;
+        directionText.text = "Direction: " + gestureDirection;
+    }
+
+
+    public void TestFunction() {
+
+
+        
+    }
+    
+    void updateTransform() {
+        float speed = Time.deltaTime * 5f;
+
+        Vector3 pos = Camera.transform.position + Camera.transform.forward;
+        gameObject.transform.position = Vector3.SlerpUnclamped(gameObject.transform.position, pos, speed);
+
+        Quaternion rot = Quaternion.LookRotation(gameObject.transform.position - Camera.transform.position);
+        gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, rot, speed);
+    }
+    #endregion
+}
